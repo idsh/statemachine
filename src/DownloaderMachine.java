d66c4418f2caee3ee7bd94253bbc9fe0113a08cb
nivdu <nivdu@post.bgu.ac.il>
2019-05-31 13:55:31 +0300

    Default Changelist

--- src/DownloaderMachine.java
+++ src/DownloaderMachine.java
@@ -3,16 +3,57 @@ public class DownloaderMachine {
     //curr state
     public ImovieDownloader currMachineState;
 
-    public int score=0;
-    public int currFreeSpace = 100;
-    public int downloadingStatus = 0;
-    public int movieSize = 0;
+    private int score=0;
+    private int currFreeSpace = 100;
+    private int downloadingStatus = 0;
+    private int movieSize = 0;
 
 
     public DownloaderMachine(){
         turnOff turnoff = new turnOff(this);
         turnOn turnon = new turnOn(this);
         currMachineState = turnoff;
+    }
+
+
+    public ImovieDownloader getCurrMachineState() {
+        return currMachineState;
+    }
+
+    public void setCurrMachineState(ImovieDownloader currMachineState) {
+        this.currMachineState = currMachineState;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getCurrFreeSpace() {
+        return currFreeSpace;
+    }
+
+    public void setCurrFreeSpace(int currFreeSpace) {
+        this.currFreeSpace = currFreeSpace;
+    }
+
+    public int getDownloadingStatus() {
+        return downloadingStatus;
+    }
+
+    public void setDownloadingStatus(int downloadingStatus) {
+        this.downloadingStatus = downloadingStatus;
+    }
+
+    public int getMovieSize() {
+        return movieSize;
+    }
+
+    public void setMovieSize(int movieSize) {
+        this.movieSize = movieSize;
     }
 
 
